f18d36d36f4b97f033f8d3acfe6c350dfadce5e0
Ramill <ramill_ibragimov@icloud.com>
2023-09-03 19:48:54 +0300

    Initial Commit

---
+++ ProgrammaticNavigationInSwiftUI/ProgrammaticNavigationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticNavigationInSwiftUIApp.swift
+//  ProgrammaticNavigationInSwiftUI
+//
+//  Created by Ramill Ibragimov on 03.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammaticNavigationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
